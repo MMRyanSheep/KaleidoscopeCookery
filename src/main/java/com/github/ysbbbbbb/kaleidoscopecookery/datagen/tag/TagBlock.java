84ee48192c3139b5b9a9f945463af4b647a6e43a
MMRyanSheep <xuruiyang114@gmail.com>
2025-10-17 20:01:47 +0800

    修复椅子不正常移除后玩家浮空问题01

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/datagen/tag/TagBlock.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/datagen/tag/TagBlock.java
@@ -52,7 +52,11 @@ public class TagBlock extends BlockTagsProvider {
                 ModBlocks.COOK_STOOL_BIRCH.get(), ModBlocks.COOK_STOOL_CHERRY.get(),
                 ModBlocks.COOK_STOOL_CRIMSON.get(), ModBlocks.COOK_STOOL_DARK_OAK.get(),
                 ModBlocks.COOK_STOOL_JUNGLE.get(), ModBlocks.COOK_STOOL_MANGROVE.get(),
-                ModBlocks.COOK_STOOL_WARPED.get());
+                ModBlocks.COOK_STOOL_WARPED.get(), ModBlocks.CHAIR_ACACIA.get(),
+                ModBlocks.CHAIR_BAMBOO.get(), ModBlocks.CHAIR_BIRCH.get(),
+                ModBlocks.CHAIR_CHERRY.get(), ModBlocks.CHAIR_CRIMSON.get(),
+                ModBlocks.CHAIR_DARK_OAK.get(), ModBlocks.CHAIR_JUNGLE.get(),
+                ModBlocks.CHAIR_MANGROVE.get(), ModBlocks.CHAIR_WARPED.get());
         this.tag(TagMod.TUNDRA_STRIDER_SPEED_BLOCKS).add(
                 Blocks.SNOW, Blocks.SNOW_BLOCK, Blocks.POWDER_SNOW,
                 Blocks.PACKED_ICE, Blocks.ICE, Blocks.FROSTED_ICE, Blocks.BLUE_ICE
